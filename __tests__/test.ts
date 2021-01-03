3d7b3cb75655163cdd557fdebd2c820e422e13ec
Sedhuraja Nallayan <sedhuait@gmail.com>
2021-01-03 23:52:16 +0530

    fix: 🐛 encodeUriComponent Fix
    special characters in the script name encoding issue fixed

--- __tests__/test.ts
+++ __tests__/test.ts
@@ -7,6 +7,14 @@ test('Get index returns data', () => {
     });
 });
 
+test('Get index returns data for FnO', () => {
+    return getIndex('SECURITIES IN F&O').then(data => {
+        expect(typeof data).toBe('object');
+        expect(data.name).toBe('SECURITIES IN F&O');
+        expect(data).toHaveProperty('data')
+    });
+});
+
 test('Get all indeces returns data', () => {
     return getIndexList().then(data => {
         expect(typeof data).toBe('object');
